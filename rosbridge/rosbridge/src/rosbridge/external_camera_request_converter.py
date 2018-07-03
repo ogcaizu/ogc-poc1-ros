2d15ed47339c46312d64dee5f93ad37416128714
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-07-03 09:22:48 +0900

    add 'external_camera_request' feature

--- rosbridge/rosbridge/src/rosbridge/external_camera_request_converter.py
+++ rosbridge/rosbridge/src/rosbridge/external_camera_request_converter.py
@@ -3,7 +3,7 @@ import datetime
 
 import pytz
 
-from rosbridge.msg import r_req as message_type
+from rosbridge.msg import c_req as message_type
 
 from rosbridge.logging import getLogger
 logger = getLogger(__name__)
@@ -13,26 +13,20 @@ def convert_mqtt_to_ros(cmd_data, ros_publisher):
     logger.infof('convert mqtt to ros, cmd_data={}', cmd_data)
 
     try:
-        r_cmd = cmd_data['r_cmd']
-        if r_cmd not in ('Navi', 'Standby', 'Clear'):
-            return 'failure: unknown r_cmd, r_cmd="{}"'.format(r_cmd)
+        c_cmd = cmd_data['c_cmd']
+        if c_cmd not in ('Monitor', 'Standby', 'Clear'):
+            return 'failure: unknown c_cmd, c_cmd="{}"'.format(c_cmd)
 
         msg = message_type()
         msg.time = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
         msg.id = int(cmd_data['id'])
-        msg.r_cmd = r_cmd
-        msg.pos.x = float(cmd_data['pos.x'])
-        msg.pos.y = float(cmd_data['pos.y'])
-        msg.pos.z = float(cmd_data['pos.z'])
+        msg.c_cmd = c_cmd
 
         ros_publisher.publish(msg)
-        result = 'success: time={time}, id={id}, r_cmd={r_cmd}, pos.x={pos_x}, pos.y={pos_y}, pos.z={pos_z}'.format(
+        result = 'success: time={time}, id={id}, c_cmd={c_cmd}'.format(
             time=msg.time,
             id=msg.id,
-            r_cmd=msg.r_cmd,
-            pos_x=msg.pos.x,
-            pos_y=msg.pos.y,
-            pos_z=msg.pos.z,
+            c_cmd=msg.c_cmd,
         )
         logger.infof(result)
     except (KeyError, ValueError) as e:
